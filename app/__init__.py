7244d044d2cd98a6cc64644073dc9327bb43db60
Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>
2020-09-15 12:10:34 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail, Message, Attachment
 
 # create an app instance
-app = Flask(__name__, static_folder='./static')
+app = Flask(__name__)
 
 # Setup the app with the config.py file
 app.config.from_object('app.config')
